b8e668845051f0428c6f9f3f5729b2dae10d17f3
Azeemuddin Aziz <azeemuddinaziz@Azeemuddins-MacBook-Air.local>
2024-08-31 18:18:11 +0530

    intialize react vite app with tailwindcss and shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button>Click Me!</Button>
+      <ModeToggle />
+    </div>
+  );
+}
+
+export default App;
