dba91cfa234311bd4c57299b5bb5f8fff2c05565
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-02 12:22:06 +0530

    fix bug: wrong implementation of AuthProvider

--- src/layouts/SimpleLayout.tsx
+++ src/layouts/SimpleLayout.tsx
@@ -1,14 +1,11 @@
 import Header from "@/components/Header";
-import { AuthProvider } from "@/context/AuthContext";
 import { Outlet } from "react-router-dom";
 
 export default function SimpleLayout() {
   return (
-    <AuthProvider>
-      <div className="flex flex-col h-screen">
-        <Header isSimple={true} />
-        <Outlet />
-      </div>
-    </AuthProvider>
+    <div className="flex flex-col h-screen">
+      <Header isSimple={true} />
+      <Outlet />
+    </div>
   );
 }
