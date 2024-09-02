34ae4fc25fe1c8ff95dca1387f5794130c2433c5
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-02 08:27:35 +0530

    cleanup: destructure data from response sent by server.

--- src/services/video.services.ts
+++ src/services/video.services.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const getAllVideos = async (query: String) => {
+const getAllVideos = async (query: String = "") => {
   try {
-    const response = await axios.get(
+    const { data } = await axios.get(
       `http://localhost:8000/api/v1/video/results?query=${query}`
     );
-    return response.data.data.docs;
+    return data.data.docs;
   } catch (error) {
     return error;
   }
@@ -13,10 +13,10 @@ const getAllVideos = async (query: String) => {
 
 const getVideoById = async (videoId: String) => {
   try {
-    const response = await axios.get(
+    const { data } = await axios.get(
       `http://localhost:8000/api/v1/video/search/${videoId}`
     );
-    return response.data.data.video;
+    return data.data.video;
   } catch (error) {
     return error;
   }
