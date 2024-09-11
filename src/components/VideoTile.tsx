b0e4986fa1a7a2bab311edf3ea870e11f2bc0957
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-11 07:54:31 +0530

    add owner details in VideoTile component and remove description.

--- src/components/VideoTile.tsx
+++ src/components/VideoTile.tsx
@@ -1,44 +1,59 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 type VideoTileProps = {
   title: string;
   thumbnail: string;
-  description: string;
+  username: string;
   views: number;
-  createdAt: string;
   id: string;
+  avatar: string;
 };
 
 function VideoTile({
   title,
   thumbnail,
-  description,
+  username,
   views,
-  createdAt,
   id,
+  avatar,
 }: VideoTileProps) {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/search/${id}`);
+  };
+
   return (
-    <Link to={`/search/${id}`}>
-      <Card className="overflow-hidden cursor-pointer">
-        <CardContent className="p-0 w-full">
-          <img
-            alt="Video thumbnail"
-            className="aspect-video object-cover w-full"
-            height="180"
-            src={thumbnail}
-            width="320"
-          />
-        </CardContent>
-        <CardFooter className="flex flex-col items-start p-4">
-          <h3 className="font-semibold">{title}</h3>
-          <p className="text-sm text-muted-foreground">{description}</p>
-          <p className="text-sm text-muted-foreground">
-            {views} • {createdAt || "No date"}
-          </p>
-        </CardFooter>
-      </Card>
-    </Link>
+    <Card onClick={handleCardClick} className="overflow-hidden cursor-pointer">
+      <CardContent className="p-0 w-full">
+        <img
+          alt="Video thumbnail"
+          className="aspect-video object-cover w-full"
+          height="180"
+          src={thumbnail}
+          width="320"
+        />
+      </CardContent>
+      <CardFooter className="flex flex-col items-start gap-2 p-4">
+        <h3 className="font-semibold">{title}</h3>
+        <Link
+          to={`/profile/${username}`}
+          className="flex items-center justify-center gap-4"
+          onClick={(e) => e.stopPropagation()} // Prevent click event from bubbling to the card
+        >
+          <Avatar className="w-6 h-6">
+            <AvatarImage src={avatar} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <span className="text-sm text-muted-foreground h-fit w-full hover:text-primary">
+            {username}
+          </span>
+        </Link>
+        <p className="text-sm text-muted-foreground">{views} • Views</p>
+      </CardFooter>
+    </Card>
   );
 }
 
