a769fe2edfdd505c6785bb9e5273120b3680fe99
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-14 17:56:53 +0530

    add playlist.services.tsx for api's related to playlist - [createPlaylist, getUserPlaylist, getPlaylistById, updatePlaylistDetails, removeVideoFromPlaylist]

---
+++ src/services/playlist.services.ts
@@ -0,0 +1,91 @@
+import axios from "axios";
+
+const createPlaylist = async (name: string, description: string) => {
+  try {
+    const { data } = await axios.post(
+      `http://localhost:8000/api/v1/playlist/create`,
+      {
+        name,
+        description,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    return data.data;
+  } catch (error) {
+    //@ts-ignore
+    return error.status;
+  }
+};
+
+const getUserPlaylist = async (userId: string) => {
+  try {
+    const { data } = await axios.get(
+      `http://localhost:8000/api/v1/playlist/getUsersPlaylists/${userId}`
+    );
+    return data.data;
+  } catch (error) {
+    //@ts-ignore
+    return error.status;
+  }
+};
+
+const getPlaylistById = async (playlistId: string) => {
+  try {
+    const { data } = await axios.get(
+      `http://localhost:8000/api/v1/playlist/getPlaylistById/${playlistId}`
+    );
+    return data.data;
+  } catch (error) {
+    //@ts-ignore
+    return error.status;
+  }
+};
+
+const updatePlaylistDetails = async (
+  name: string,
+  description: string,
+  playlistId: string
+) => {
+  try {
+    const { data } = await axios.patch(
+      `http://localhost:8000/api/v1/playlist/updatePlaylist/${playlistId}`,
+      {
+        name,
+        description,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    return data.data;
+  } catch (error) {
+    //@ts-ignore
+    return error.status;
+  }
+};
+
+const removeVideoFromPlaylist = async (playlistId: string, videoId: string) => {
+  try {
+    const { data } = await axios.patch(
+      `http://localhost:8000/api/v1/playlist/removeVideo/${playlistId}/${videoId}`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+    return data.data;
+  } catch (error) {
+    //@ts-ignore
+    return error.status;
+  }
+};
+
+export {
+  createPlaylist,
+  getUserPlaylist,
+  getPlaylistById,
+  updatePlaylistDetails,
+  removeVideoFromPlaylist,
+};
