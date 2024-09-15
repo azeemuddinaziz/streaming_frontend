a73842b382252ff55e4e4ef06ab279470a5a1665
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-15 09:52:05 +0530

    update Subscriptions page with importing home component and adding a todo.

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -1,55 +1,8 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
-import VideoTile from "@/components/VideoTile";
-import { getAllVideos } from "@/services/video.services";
-import { useEffect, useState } from "react";
-
-type Video = {
-  thumbnail: string;
-  title: string;
-  username: string;
-  views: number;
-  _id: string;
-};
+import Home from "./Home";
 
 function Subscriptions() {
-  const [videos, setVideos] = useState<Video[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      //TODO: make this working as getting all videos of channel subscribed.
-      setVideos(await getAllVideos());
-    })();
-  }, []);
-
-  return (
-    <ScrollArea>
-      <div className="grid md:grid-cols-4 gap-4 p-2 ">
-        {videos.length > 0 ? (
-          videos.map((video) => {
-            return (
-              <VideoTile
-                key={video._id}
-                title={video.title}
-                thumbnail={video.thumbnail}
-                //@ts-ignore
-                username={video.ownerDetails.username}
-                //@ts-ignore
-                avatar={video.ownerDetails.avatar}
-                views={video.views}
-                id={video._id}
-              />
-            );
-          })
-        ) : (
-          <div>
-            <div className="text-gray-500">
-              Something went wrong while loading videos.
-            </div>
-          </div>
-        )}
-      </div>
-    </ScrollArea>
-  );
+  //TODO: Make this funcitonal. Only videos from subscribed channels.
+  return <Home />;
 }
 
 export default Subscriptions;
