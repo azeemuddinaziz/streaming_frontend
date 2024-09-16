4cd42351528c9b925ceef28306768dc187dc0585
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-16 16:32:01 +0530

    update: port to 3000 as vercel local backend runs over prot 3000.

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ import PlaylistView from "./pages/PlaylistView.tsx";
 import { Toaster } from "sonner";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api/v1";
+axios.defaults.baseURL = "http://localhost:3000/api/v1";
 
 const router = createBrowserRouter([
   {
